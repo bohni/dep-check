83146f5c5e6a38cb4f07b3fa37578f7180df3ed3
Stefan Bohn <stefan.bohn@shks.de>
2020-04-24 12:38:57 +0200

    Add empty spring project
    Create an simple Maven project with Java on https://start.spring.io
    using Spring version 2.2.6, Packaging war and Java version 8.

---
+++ src/main/java/net/bohni/depcheck/DepCheckApplication.java
@@ -0,0 +1,13 @@
+package net.bohni.depcheck;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DepCheckApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DepCheckApplication.class, args);
+	}
+
+}
---
+++ src/test/java/net/bohni/depcheck/DepCheckApplicationTests.java
@@ -0,0 +1,13 @@
+package net.bohni.depcheck;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DepCheckApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
